36d3d0cdc3d02d84aec741ee13b48135ba8d58fe
Paul Okenne <64801950+TheUcheojor@users.noreply.github.com>
2020-12-10 11:30:30 -0500

    Delete project_header.h

--- project_header.h
+++
@@ -1,8 +0,0 @@
-#ifndef TERM_PROJECT_HEADER_H
-#define TERM_PROJECT_HEADER_H
-
-void configurePorts(void);//configures PORT1 (Pin 0,1,& 4) and PORT2(Pin 0)
-void configureInterrupts(void);//configure interrupts for  PORT1 (Pin 1,& 4)
-void configureNVIC(void);//configure interrupt at NVIC level for  PORT1
-void configureUART(void);//Configure UART
-#endif
